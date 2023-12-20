4cfa67e1e106d25dd1064a9d243092f22fe7bc54
danielmachado <danielmachadopintos@gmail.com>
2023-12-19 22:41:26 -0300

    Update dependencies in mix.exs

--- mix.exs
+++ mix.exs
@@ -33,9 +33,9 @@ defmodule Scheduler.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.7.10"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
+      {:mongodb, "~> 1.0"},
+      {:mongodb_ecto, "~> 1.1"},
+      {:poolboy, "~> 1.5.2"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.1"},
