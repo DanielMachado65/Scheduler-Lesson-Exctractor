20e257fed7d20ddb2c6f4d516f9d35734a6aa259
danielmachado <danielmachadopintos@gmail.com>
2023-12-19 22:41:33 -0300

    Remove unnecessary code in setup_sandbox function

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -36,8 +36,6 @@ defmodule Scheduler.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Scheduler.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
   end
 
   @doc """
