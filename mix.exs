2e8e53df2254dd17c20bbfdb796d36ca8c31dcea
danielmachado <danielmachadopintos@gmail.com>
2023-12-22 09:52:53 -0300

    Remove ecto.migrate from ecto.setup command

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule SchedulerLesson.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
