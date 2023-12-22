5ba90c497626111875119d10e14fb1d65f2b0cac
danielmachado <danielmachadopintos@gmail.com>
2023-12-21 22:14:46 -0300

    Remove commented out code in seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,7 +5,7 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     SchedulerLesson.Repo.insert!(%SchedulerLesson.SomeSchema{})
+# SchedulerLesson.Repo.insert!(%SchedulerLesson.SomeSchema{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
