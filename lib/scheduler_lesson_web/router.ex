1ddd8c4a9549030fe6e61a69f36f40b18eadc105
danielmachado <danielmachadopintos@gmail.com>
2024-01-24 23:11:11 -0300

    Move health_check route to the API scope

--- lib/scheduler_lesson_web/router.ex
+++ lib/scheduler_lesson_web/router.ex
@@ -5,9 +5,9 @@ defmodule SchedulerLessonWeb.Router do
     plug :accepts, ["json"]
   end
 
-  get "/health_check", HealthCheckController, :index
-
   scope "/api", SchedulerLessonWeb do
     pipe_through :api
+
+    get "/health_check", HealthCheckController, :index
   end
 end
